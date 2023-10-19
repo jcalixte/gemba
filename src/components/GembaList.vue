7934db3fd3ce5f2b253f1127ddb3ec7214daf3df
Julien Calixte <julienc@bam.tech>
2023-10-19 17:08:01 +0200

    simplify template

--- src/components/GembaList.vue
+++ src/components/GembaList.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import { useGembaStore } from '@/stores/gemba.store'
+import { computed } from 'vue'
 
 const gembaStore = useGembaStore()
+const gembas = computed(() => gembaStore.gembas)
 </script>
 
 <template>
   <div class="gemba-list">
     <ul>
-      <li v-for="gemba in gembaStore.gembas" :key="gemba.id">
+      <li v-for="gemba in gembas" :key="gemba.id">
         {{ gemba.title }}
       </li>
     </ul>
